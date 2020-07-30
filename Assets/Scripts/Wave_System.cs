4fcb44b34797e02b2b4c36ee1799f117c38990a4
BenSoars <61790906+BenSoars@users.noreply.github.com>
2020-07-30 22:16:45 +0100

    Commented Wave System

--- Assets/Scripts/Wave_System.cs
+++ Assets/Scripts/Wave_System.cs
@@ -6,18 +6,20 @@ using UnityEngine.UI;
 public class Wave_System : MonoBehaviour
 {
     //Ben Soars
-    public List<GameObject> enemyTypes = new List<GameObject>();
-    public List<Transform> spawnPoints = new List<Transform>();
+    public List<GameObject> enemyTypes = new List<GameObject>(); // the enemy types
+    public List<Transform> spawnPoints = new List<Transform>(); // the amount of enemies per wave
 
-    public List<string> amountOf = new List<string>();
-    public List<GameObject> spawnedEnemies = new List<GameObject>();
-    public int enemiesLeft;
+    public List<string> amountOf = new List<string>(); 
+    public List<GameObject> spawnedEnemies = new List<GameObject>(); // the spawned enemies
+    public int enemiesLeft;  // the amount of enemies remaining
 
-    public int curRound;
-    public List<int> enemyArray = new List<int>();
-    public bool m_startedWaves;
+    public int curRound;  // the current round the player is on
+    public List<int> enemyArray = new List<int>();  // the current array of enemies
+    public bool m_startedWaves; // check for the start of the wave
 
     //Kurtis Watson
+
+    // component access
     private Player_Controller r_playerController;
     private User_Interface r_userInterface;
     private Prototype_Classes r_prototypeClasses;
@@ -38,6 +40,7 @@ public class Wave_System : MonoBehaviour
     //Kurtis Watson
     private void Start()
     {
+        // get access to the various components needed by this script
         m_wispPoint = GameObject.FindGameObjectsWithTag("WispPoint");
         r_playerController = FindObjectOfType<Player_Controller>();
         r_userInterface = FindObjectOfType<User_Interface>();
@@ -63,54 +66,57 @@ public class Wave_System : MonoBehaviour
     {
         m_newWave = false;
         yield return new WaitForSeconds(m_intermissionTime);
-        m_startedWaves = true;
-        f_sortOutEnemys();        
+        m_startedWaves = true; //Update UI values.
+        f_sortOutEnemys(); //Spawn enemies of different types.
         for (int k = 0; k < enemyArray.Count; k++)
         {
             for (int i = 0; i < enemyArray[k]; i++)
             {
                 m_random = Random.Range(0, 4);
-                GameObject spawned = Instantiate(m_wisps[k], m_wispPoint[m_random].transform.position, Quaternion.identity);
-                spawnedEnemies.Add(spawned);
+                GameObject spawned = Instantiate(m_wisps[k], m_wispPoint[m_random].transform.position, Quaternion.identity); //Spawn wisps at a random point.
+                spawnedEnemies.Add(spawned); //Add enemy spawned to enemies spawned list.
             }
         }
-        enemiesLeft = spawnedEnemies.Count;
-        r_prototypeClasses.m_fogStrength = 0.2f; //THIS WILL NEED TO BE CHANGED IN ORDER TO ADD INTERMISSION BETWEEN ROUNDS.
-        r_prototypeClasses.m_currentFog = 0.2f;
-        r_prototypeClasses.m_stonePower[r_prototypeClasses.m_chosenBuff] -= enemiesLeft * 2;
-        m_fogMath = r_prototypeClasses.m_fogStrength / enemiesLeft;
-        curRound += 1;
+        enemiesLeft = spawnedEnemies.Count; //Set the count for the enemies left.
+        r_prototypeClasses.m_fogStrength = 0.2f; //Fog strength.
+        r_prototypeClasses.m_currentFog = 0.2f; //Reset current fog.
+        r_prototypeClasses.m_stonePower[r_prototypeClasses.m_chosenBuff] -= enemiesLeft * 2; //Decrease the chosen enemy buff by two times the amount of enemies.
+        m_fogMath = r_prototypeClasses.m_fogStrength / enemiesLeft; //Calculate the amount of fog to decrease each enemy kill.
+        curRound += 1; //Increase current round by one.
+       
         m_timeMet = false;
     }
 
     //Kurtis Watson
     void f_updateUI()
     {
-        m_enemyCount.text = ("" + enemiesLeft);
+        m_enemyCount.text = ("" + enemiesLeft); //Display enemies left on the runtime UI.
     }
 
     // Update is called once per frame
+    //Ben Soars
     void FixedUpdate()
     {       
-        //Ben Soars
+        
         if (spawnedEnemies.Count <= 0 && enemiesLeft == 0 && m_timeMet == false)
         {
             r_notesSystem.m_spawnNote = true;
-            m_startedWaves = false;
-            r_prototypeClasses.m_canSelect = true;
-            r_prototypeClasses.m_activeStone[r_prototypeClasses.m_classState] = false;
-            m_timeMet = true;
-            r_userInterface.f_waveTimer();
-            f_spawnWisps();
+            m_startedWaves = false; //Begin the wave (required for a different script).
+            r_prototypeClasses.m_canSelect = true; //Allow the player to choose a new Starstone.
+            r_prototypeClasses.m_activeStone[r_prototypeClasses.m_classState] = false; //Disable the current stone so that it can be chosen again.
+            m_timeMet = true; // time has been met
+            r_userInterface.f_waveTimer(); //Update the round time limit.
+            f_spawnWisps(); // spawn more wisps
         }
         else
         {
-            for (int i = 0; i < spawnedEnemies.Count; i++)
+            // if there are enemies
+            for (int i = 0; i < spawnedEnemies.Count; i++) //check the list of enemies that are spawned
             {
-                if (spawnedEnemies[i] == null)
+                if (spawnedEnemies[i] == null) // if the enemy at that point doesn't exsist
                 {
-                    spawnedEnemies.RemoveAt(i);
-                    break;
+                    spawnedEnemies.RemoveAt(i); // remove eveny from the list
+                    break; // break the loop, as the for loop won't work due to an element being removed
                 }
             }
         }
@@ -119,10 +125,10 @@ public class Wave_System : MonoBehaviour
     //Ben Soars
     void f_sortOutEnemys()
     {
-        string[] varArray = amountOf[curRound].Split('_');
+        string[] varArray = amountOf[curRound].Split('_'); // split the current amount of enemies
 
         enemyArray.Clear();
-        for (int i = 0; i < enemyTypes.Count; i++)
+        for (int i = 0; i < enemyTypes.Count; i++)  // for loop for all the enemy types
         {
             enemyArray.Add(System.Convert.ToInt32(varArray[i]));// convert the string into a string if it can
         }
